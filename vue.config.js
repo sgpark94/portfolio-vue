fdb3818c5c33d2693a8f7b3a32363e53b1e75c38
sgpark94 <olloyy@naver.com>
2024-01-06 12:35:41 +0900

    build

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+// const { defineConfig } = require('@vue/cli-service')
+module.exports = {
   transpileDependencies: true,
   lintOnSave: false,
   publicPath:'/portfolio-vue/',
   outputDir:'docs',
   // outputDir: 빌드한 파일을 저장하는 경로, docs 폴더로 지정한다.
   // publicPath: 배포된 페이지의 URL 설정.
-})
+}
