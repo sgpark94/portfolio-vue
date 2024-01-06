de40cafd1fee1632cd961975234bb4415f6d7892
sgpark94 <olloyy@naver.com>
2024-01-06 12:33:40 +0900

    build

--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,6 @@ module.exports = defineConfig({
   lintOnSave: false,
   publicPath:'/portfolio-vue/',
   outputDir:'docs',
+  // outputDir: 빌드한 파일을 저장하는 경로, docs 폴더로 지정한다.
+  // publicPath: 배포된 페이지의 URL 설정.
 })
