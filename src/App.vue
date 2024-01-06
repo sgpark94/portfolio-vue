7c3dcc25e4811663ba5f24d9448d2dd748b3060c
sgpark94 <olloyy@naver.com>
2024-01-06 12:44:29 +0900

    MOD - body overflow hidden 처리

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,9 @@ export default {
 </script>
 
 <style>
+body {
+  overflow: hidden;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -38,16 +41,13 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-
 nav {
   padding: 30px;
 }
-
 nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 nav a.router-link-exact-active {
   color: #42b983;
 }
